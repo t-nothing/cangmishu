5fe1c27291aa894d40c738bb4a2c0d73f36a6713
llf <18013272501@163.com>
2019-06-28 17:31:22 +0800

    修改console

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -27,8 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-
-        $schedule->job(new WarningEmailJob())->everyFiveMinutes();
+        
     }
 
     /**
