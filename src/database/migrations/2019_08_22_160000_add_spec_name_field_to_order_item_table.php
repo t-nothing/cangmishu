c679d5c9ca803ac590a85cc7cf9fc6b24112e66b
胡斌杰 <hubinjie@live.cn>
2019-08-22 19:45:09 +0800

    test

--- src/database/migrations/2019_08_22_160000_add_spec_name_field_to_order_item_table.php
+++ src/database/migrations/2019_08_22_160000_add_spec_name_field_to_order_item_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddSecNameFieldToOrderItemTable extends Migration
+class AddSpecNameFieldToOrderItemTable extends Migration
 {
     /**
      * Run the migrations.
