441b5e9434bb74b525af180e0d0a5b2ec385f776
胡斌杰 <hubinjie@live.cn>
2019-08-30 18:34:30 +0800

    加大显示红色

--- src/resources/views/emails/verifyCodeMail.blade.php
+++ src/resources/views/emails/verifyCodeMail.blade.php
@@ -11,7 +11,7 @@
         <td colspan="2" style="padding: 50px 30px 10px 30px; color: #7f7f7f;  width:760px; line-height: 30px;word-wrap:break-word">
             Hi 你好,<br/>
             <br>
-            欢迎使用仓秘书系统, 您的验证码为：{{$code}},此验证码有效期为5分钟，请尽快使用。<br/>
+            欢迎使用仓秘书系统, 您的验证码为：<span style="font-size: 18px; font-weight: bolder; color: red;">{{$code}}</span>,此验证码有效期为5分钟，请尽快使用。<br/>
             <div style="padding-top: 30px">
                 <span>此邮件由系统自动发送，请勿直接回复。</span>
             </div>
