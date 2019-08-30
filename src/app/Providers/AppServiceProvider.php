a809f04196aa571a1f4781bdb79f8b1ed419a83a
胡斌杰 <hubinjie@live.cn>
2019-08-30 12:04:44 +0800

    注册已经跑通

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -81,7 +81,7 @@ class AppServiceProvider extends ServiceProvider
             return $guard;
         });
 
-        \Validator::extend('mobile', function ($attribute, $value, $parameters, Validator $validator) {
+        \Validator::extend('mobile', function ($attribute, $value, $parameters, $validator) {
             return $validator->validateRegex($attribute, $value, ['/^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\d{8}$/']);
         });
 
