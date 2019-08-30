2c87c244033732f463f6329d87a2c51f7521a301
胡斌杰 <hubinjie@live.cn>
2019-08-30 14:27:37 +0800

    装个horizon

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -25,6 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('horizon:snapshot')->everyFiveMinutes();
         // $schedule->command('inspire')
         //          ->hourly();
 
