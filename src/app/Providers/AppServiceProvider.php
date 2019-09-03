3d3aae8f7feec8d2348f9f8987501990120015bd
胡斌杰 <hubinjie@live.cn>
2019-09-03 12:27:39 +0800

    test

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -79,15 +79,12 @@ class AppServiceProvider extends ServiceProvider
             );
 
             $app->refresh('request', $guard, 'setRequest');
-
             return $guard;
         });
 
         \Validator::extend('mobile', function ($attribute, $value, $parameters, $validator) {
             return $validator->validateRegex($attribute, $value, ['/^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\d{8}$/']);
         });
-
-
         Schema::defaultStringLength(191);
     }
 
