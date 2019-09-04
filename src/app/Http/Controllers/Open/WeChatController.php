9bddcd2eb30d1e564ca11eaea9087a6d5418832e
胡斌杰 <hubinjie@live.cn>
2019-09-04 19:47:28 +0800

    增加扫码登录

--- src/app/Http/Controllers/Open/WeChatController.php
+++ src/app/Http/Controllers/Open/WeChatController.php
@@ -16,14 +16,9 @@ class WeChatController extends Controller
      *
      * @return string
      */
-    public function serve()
     {
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
-        $app = app('wechat.mini_program');
-        $app->server->push(function($message){
-            return "欢迎关注 overtrue！";
-        });
 
         return $app->server->serve();
     }
