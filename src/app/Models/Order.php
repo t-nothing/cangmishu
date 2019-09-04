9d42a01ceeefb28f9ca923522810130156160790
胡斌杰 <hubinjie@live.cn>
2019-09-04 18:22:58 +0800

    test

--- src/app/Models/Order.php
+++ src/app/Models/Order.php
@@ -137,7 +137,7 @@ class Order extends Model
                 $name = '订单已取消';
                 break;
             case Order::STATUS_DEFAULT:
-                $name = '待审核';
+                $name = '待确认';
                 break;
             case Order::STATUS_PICKING:
                $name = '拣货中';
