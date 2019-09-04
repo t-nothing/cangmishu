50ae755a6b4fbfbbad9c410387a6f04a98bdb6b2
胡斌杰 <hubinjie@live.cn>
2019-09-04 19:47:28 +0800

    增加扫码登录

--- src/app/Providers/EventServiceProvider.php
+++ src/app/Providers/EventServiceProvider.php
@@ -13,6 +13,9 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
+        'App\Events\WechatScanLogined' => [
+                'App\Listeners\WechatScanLoginedNotification',
+            ],
         'App\Events\StockLocationIn' => [
                 'App\Listeners\StockLocationInNotification',
                 'App\Listeners\StockLocationInLogNotification',
