f97cff6b58f80958aefaf651896984032e920f0b
胡斌杰 <hubinjie@live.cn>
2019-09-11 14:18:50 +0800

    test

--- src/app/Http/Controllers/Open/WeChatController.php
+++ src/app/Http/Controllers/Open/WeChatController.php
@@ -92,7 +92,7 @@ class WeChatController extends Controller
      *
      * @return string
      */
-    public function serve($id = 'mini_program')
+    public function serve(BaseRequests $request, $id = 'mini_program')
     {
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
