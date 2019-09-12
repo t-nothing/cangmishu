a6c347eb889a5ac5307506e31201c68764cbd18d
胡斌杰 <hubinjie@live.cn>
2019-09-12 23:01:22 +0800

    test

--- src/app/Http/Controllers/Open/SwiperController.php
+++ src/app/Http/Controllers/Open/SwiperController.php
@@ -18,13 +18,13 @@ class SwiperController extends Controller
             "id"            =>  1,
             "caption"       =>  "合作 谁是你的菜",
             "link_url"      =>  "",
-            "image_url"     =>  public_path('images/swiper/8e50c65fda145e6dd1bf4fb7ee0fcecc.jpg'),
+            "image_url"     =>  resource_path('images/swiper/8e50c65fda145e6dd1bf4fb7ee0fcecc.jpg'),
         ];
         $arr[] = [
             "id"            =>  2,
             "caption"       =>  "活动 美食节",
             "link_url"      =>  "",
-            "image_url"     =>  public_path('images/swiper/65091eebc48899298171c2eb6696fe27.jpg'),
+            "image_url"     =>  storage_path('images/swiper/65091eebc48899298171c2eb6696fe27.jpg'),
         ];
         $arr[] = [
             "id"            =>  3,
