036415ab614ed78e5ffe4005efc102d6bb82cfe1
胡斌杰 <hubinjie@live.cn>
2020-10-18 18:53:52 +0800

    log

--- src/app/Listeners/OrderCreatedNotification.php
+++ src/app/Listeners/OrderCreatedNotification.php
@@ -99,7 +99,7 @@ class OrderCreatedNotification  implements ShouldQueue
 
                     //这里是发给服务号的
                     $result = $app->template_message->send($data);
-                    app('log')->info('发送结果成功');
+                    app('log')->info('发送结果成功', $data);
                 }
                 catch(InvalidArgumentException $ex)
                 {
