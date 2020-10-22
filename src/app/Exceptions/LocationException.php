8e7d23e5c9ca03561448c28b55e7d65b1d920600
胡斌杰 <hubinjie@live.cn>
2020-10-22 11:31:05 +0800

    test

--- src/app/Exceptions/LocationException.php
+++ src/app/Exceptions/LocationException.php
@@ -4,4 +4,17 @@ namespace App\Exceptions;
 
 class LocationException extends \Exception
 {
+    var $locations;
+
+    public function __construct(array $locations){
+        $this->locations = $locations;
+        parent::__construct(trans("message.warehouseLocationNotExistExt", [
+                'code'=> implode(",", $locations)
+            ]), 0);
+    }   
+
+
+    public function getLocations() {
+        return $this->locatiosn;
+    }
 }
