fb5b2ae2a169083c306c33cd6d6bdad130faa14a
胡斌杰 <hubinjie@live.cn>
2020-10-26 14:39:59 +0800

    增加了两个新字段

--- src/app/Http/Requests/UpdateReceiverAddressRequest.php
+++ src/app/Http/Requests/UpdateReceiverAddressRequest.php
@@ -34,7 +34,9 @@ class UpdateReceiverAddressRequest extends BaseRequests
             'city'     => 'required|string',
             'district' => 'required|string',
             'address'  => 'required|string',
-            'postcode' => 'string'
+            'postcode' => 'string',
+            'street'    => 'string',
+            'door_no'   => 'string',
         ];
     }
 }
