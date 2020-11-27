4484316c293de4b177edbe7ee1899160386874d3
HuoZhangqi <h947136@qq.com>
2020-11-27 16:12:17 +0800

    fix

--- src/app/Http/Controllers/OrderController.php
+++ src/app/Http/Controllers/OrderController.php
@@ -621,7 +621,7 @@ class OrderController extends Controller
         try {
             Storage::put('1.html', view('pdfs.order.template_pick', ['order' => $order->toArray()])->toHtml());
         } catch (\Throwable $throwable) {
-            info('保存失败');
+            info('保存失败', ['throwable' => $throwable->getMessage()]);
         }
         
         if($request->filled("require_url") && $request->require_url == 1) {
