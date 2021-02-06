fcc1b5106acc019b3c09f611f7089b44e0b3eda6
胡斌杰 <hubinjie@live.cn>
2021-02-06 14:01:43 +0800

    增加注释

--- src/app/Http/Controllers/SalesDataController.php
+++ src/app/Http/Controllers/SalesDataController.php
@@ -1,5 +1,12 @@
 <?php
-
+/*
+ * 仓秘书免费开源WMS仓库管理系统+订货订单管理系统
+ *
+ * (c) Hunan NLE Network Technology Co., Ltd. <cangmishu.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace App\Http\Controllers;
 
 use App\Services\StatisticsService;
